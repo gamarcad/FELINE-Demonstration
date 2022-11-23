7f1e84849657107981ed1454be33b0e4608bceb3
Anonymous <marcadet.gael@gmail.com>
2022-11-23 17:35:01 +0100

    Suppression of the unused launch.sh file and a new downloading data script

---
+++ linear-api/download-data.sh
@@ -0,0 +1,8 @@
+# --------------------------------------------
+# file: download-data.sh
+# description: download the data directly from a distant cloud.
+#   It allows to avoid the data processing that can be long.
+# --------------------------------------------
+
+wget https://drive.uca.fr/f/e77ab24f6e704e949068/
+unzip data.zip
